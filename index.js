675abbb3c8d451cdaa21dbc57a5372ffca27d623
Shraddha Rao <shraddharaom@gmail.com>
2022-11-21 21:52:45 -0600

    commit tree f00d8cf28b441da3197488ad8498687355369210

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [(returnFirstTwoDrivers), (returnLastTwoDrivers)];
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+       return integer * fare;
+}
+}
+
+const fareDoubler = function(fare) {
+    return fare * 2;
+}
+
+const fareTripler = function(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    return drivers,selectingDrivers(returnFirstTwoDrivers) ||
+           drivers,selectingDrivers(returnLastTwoDrivers);
+    }
